c8046382c2303c7c6b88ef2efab375dee098c9f4
ama101997 <131978987+ama101997@users.noreply.github.com>
2023-04-27 16:51:17 +0200

    Update voiture.py

--- voiture.py
+++ voiture.py
@@ -1,8 +1,8 @@
-class Voiture(Vehicule):
-    nb_voitures = 0
-
-    def __init__(self, modele: str, nb_roues: int, couleurs: list, plaque: str, marque: str, annee: int):
-        super().__init__(modele, nb_roues, couleurs, plaque, "Voiture")
-        self.marque = marque
-        self.annee = annee
-        Voiture.nb_voitures += 1
+from vehicle import Vehicl
+class Car(Vehicle):
+def _init_(self, make, model, year, color, num_doors, fuel_type):
+super()._init_(make, model, year, color)
+self.num_doors = num_doors
+self.fuel_type = fuel_type
+def _str_(self):
+return f"{super()._str_()}, {self.num_doors} portes, {self.fuel_type}"
