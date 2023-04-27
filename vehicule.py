693aab67e7da9f57c22114b320bd25834af76f50
ama101997 <131978987+ama101997@users.noreply.github.com>
2023-04-27 16:50:28 +0200

    Update vehicule.py

--- vehicule.py
+++ vehicule.py
@@ -1,11 +1,8 @@
 class Vehicle:
-    def __init__(self, vehicle_type, brand, model, year, color, license_plate):
-        self.vehicle_type = vehicle_type
-        self.brand = brand
-        self.model = model
-        self.year = year
-        self.color = color
-        self.license_plate = license_plate
-
-    def __str__(self):
-        return f"{self.vehicle_type} {self.brand} {self.model} ({self.color}, {self.year}) immatriculé {self.license_plate}"
+def _init_(self, make, model, year, color):
+self.make = make
+self.model = model
+self.year = year
+self.color = color
+def _str_(self):
+return f"{self.year} {self.make} {self.model} ({self.color})"
