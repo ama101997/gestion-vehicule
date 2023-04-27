3b22f4c7040adb0dbb55e138fc97a1df37ab2bda
ama101997 <131978987+ama101997@users.noreply.github.com>
2023-04-27 16:23:48 +0200

    Update vehicule.py

--- vehicule.py
+++ vehicule.py
@@ -1,50 +1,11 @@
-class Vehicule:
-    nom_application = "Gestion des véhicules"
+class Vehicle:
+    def __init__(self, vehicle_type, brand, model, year, color, license_plate):
+        self.vehicle_type = vehicle_type
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.color = color
+        self.license_plate = license_plate
 
-    def __init__(self, modele: str, nb_roues: int, couleurs: list, plaque: str, type_vehicule: str, jantes: bool = False):
-        self.modele = modele
-        self.nb_roues = nb_roues
-        self.couleurs = couleurs
-        self.plaque = plaque
-        self.type_vehicule = type_vehicule
-        self.jantes = jantes
-
-
-        super().__init__(modele, nb_roues, couleurs, plaque, "Voiture")
-        self.marque = marque
-        self.annee = annee
-        Voiture.nb_voitures += 1
-
-class Moto(Vehicule):
-    nb_motos = 0
-
-    def __init__(self, modele: str, nb_roues: int, couleurs: list, plaque: str, marque: str, annee: int, sidecar: bool = False):
-        super().__init__(modele, nb_roues, couleurs, plaque, "Moto")
-        self.marque = marque
-        self.annee = annee
-        self.sidecar = sidecar
-        Moto.nb_motos += 1
-
-class Bus(Vehicule):
-    nb_bus = 0
-
-    def __init__(self, modele: str, nb_roues: int, couleurs: list, plaque: str, nb_places: int, annee: int):
-        super().__init__(modele, nb_roues, couleurs, plaque, "Bus")
-        self.nb_places = nb_places
-        self.annee = annee
-        Bus.nb_bus += 1
-
-    
-    def set_nb_roues(self, nb_roues):
-        self.nb_roues = nb_roues
-        
-    def set_plaque_immatriculation(self, plaque_immatriculation):
-        self.plaque_immatriculation = plaque_immatriculation
-        
-    def set_type_vehicule(self, type_vehicule):
-        self.type_vehicule = type_vehicule
-        
     def __str__(self):
-        return f"{self.marque} {self.modele} ({self.annee}) de couleur {self.couleur}"
-        
-        
+        return f"{self.vehicle_type} {self.brand} {self.model} ({self.color}, {self.year}) immatriculé {self.license_plate}"
